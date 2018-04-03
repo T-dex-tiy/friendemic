fc9b631ab8b2e790302032e985363bdbb92b19b4
T-Dex <dex.m.mills@gmail.com>
2018-04-02 19:13:59 -0600

    CSS turnary

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -8,16 +8,23 @@ class UserList extends Component {
     const fullList = Object.keys(this.props.User).map(
       key => this.props.User[key]
     );
-    const renderedList = Object.keys(fullList).map(key => {
-      return fullList[key];
+    const renderedList = Object.keys(fullList).map(key => fullList[key]);
+    const statusName = Object.keys(fullList).map(key => {
+      return fullList[key].available ? 'listIems online' : 'listItems offline';
     });
+
     return (
-      <div className="userlist">
+      <div className="listItems">
         {Object.keys(renderedList).map(key => {
           return (
-            <div>
+            <div
+              className={
+                renderedList[key].available
+                  ? 'listItems online'
+                  : 'listItems offline'
+              }
+            >
               <h2>{renderedList[key].Name}</h2>
-              <h4>{renderedList[key].available}</h4>
               <h4>{renderedList[key].location}</h4>
               <h4>{renderedList[key].email}</h4>
             </div>
